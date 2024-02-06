89a34c21aab8525369cf8ab4371f4f829b54a03d
masakinihirota <masakinihirota@gmail.com>
2024-02-07 04:42:57 +0900

    移動終了 認証に不具合

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  compiler: {
-    removeConsole: {
-      exclude: ['error'],
-    },
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
+
+
+// console.logなどを削除する。エラーのみ残す
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   compiler: {
+//     removeConsole: {
+//       exclude: ['error'],
+//     },
+//   },
+// };
+
+// module.exports = nextConfig;
---
+++ src/app/(app)/page.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@nextui-org/button";
+import { cookies } from "next/headers";
+import Link from "next/link";
+
+import AuthButton from "@/components/Auth/AuthButton";
+import { ThemeSwitcher } from "@/components/darkmode/ThemeSwitcher";
+import { createServerClient } from "@/lib/supabase-server";
+
+export default async function Home() {
+  const cookieStore = cookies();
+  const supabase = createServerClient();
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  return (
+    <div className="">
+      <nav className="flex flex-row items-center justify-between ">
+        Home
+        {/* Dark mode 切り替えスイッチ */}
+        <ThemeSwitcher />
+        {/* OAuth用 */}
+        <Link href="/auth">OAuthLoginButton</Link>
+        {/* メール用 */}
+        <AuthButton />
+        {/* Sign out */}
+        <form className="block" action="/auth/signout" method="post">
+          <button type="submit">Sign out</button>
+        </form>
+      </nav>
+      <h1>Welcome {session?.user?.email}</h1>
+      <div>
+        <Button color="default">Default</Button>
+        <Button color="primary">Primary</Button>
+        <Button color="secondary">Secondary</Button>
+        <Button color="success">Success</Button>
+        <Button color="warning">Warning</Button>
+        <Button color="danger">Danger</Button>
+      </div>
+      footer
+    </div>
+  );
+}
