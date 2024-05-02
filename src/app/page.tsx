a2b36b3fa427f1d5ab3dd5a3286812dae57f96fb
masakinihirota <masakinihirota@gmail.com>
2024-05-02 19:05:25 +0900

    ESLint Prettierの追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Page() {
     <div className="">
       TOP PAGE
       {/* <PricingPage /> */}
+      <div className="flex h-24 p-3 m-4 border-2 border-gray-300 lg:p-4 lg:m-4"></div>
     </div>
   );
 }
