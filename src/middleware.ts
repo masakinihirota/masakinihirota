e817d99f28aff28239f2b7036b9709eac4f73d31
masakinihirota <masakinihirota@gmail.com>
2024-05-03 21:00:59 +0900

    Refactor middleware to support i18n and fix language switcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,7 +19,8 @@ export async function middleware(req: NextRequest) {
 
   // 言語の取得
   let lng;
-  if (req.cookies?.has(cookieName)) lng = acceptLanguage.get(req.cookies.get(cookieName)?.value);
+  if (req.cookies?.has(cookieName))
+    lng = acceptLanguage.get(req.cookies.get(cookieName)?.value);
   if (!lng) lng = acceptLanguage.get(req.headers.get('Accept-Language'));
   if (!lng) lng = fallbackLng;
 
@@ -32,17 +33,21 @@ export async function middleware(req: NextRequest) {
     req.nextUrl.pathname !== '/signin/password_signin' &&
     req.nextUrl.pathname !== '/auth/callback' &&
     req.nextUrl.pathname !== '/account' &&
-    !languages.some(loc => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
+    !languages.some((loc) => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
     !req.nextUrl.pathname.startsWith('/_next')
   ) {
-    return NextResponse.redirect(new URL(`/${lng}${req.nextUrl.pathname}`, req.url));
+    return NextResponse.redirect(
+      new URL(`/${lng}${req.nextUrl.pathname}`, req.url)
+    );
   }
 
   if (req.headers.has('referer')) {
     const referer = req.headers.get('referer');
     if (referer) {
       const refererUrl = new URL(referer);
-      const lngInReferer = languages.find(l => refererUrl.pathname.startsWith(`/${l}`));
+      const lngInReferer = languages.find((l) =>
+        refererUrl.pathname.startsWith(`/${l}`)
+      );
 
       const response = NextResponse.next();
       if (lngInReferer) response.cookies.set(cookieName, lngInReferer);
@@ -52,4 +57,3 @@ export async function middleware(req: NextRequest) {
 
   return NextResponse.next();
 }
-
