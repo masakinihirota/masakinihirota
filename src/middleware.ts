6a4afe13e9803d9c1381685644912778d33a4dfe
masakinihirota <masakinihirota@gmail.com>
2024-05-03 21:00:24 +0900

    chore: Refactor middleware to support i18n and fix language switcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,29 +19,30 @@ export async function middleware(req: NextRequest) {
 
   // 言語の取得
   let lng;
-  if (req.cookies?.has(cookieName))
-    lng = acceptLanguage.get(req.cookies.get(cookieName)?.value);
+  if (req.cookies?.has(cookieName)) lng = acceptLanguage.get(req.cookies.get(cookieName)?.value);
   if (!lng) lng = acceptLanguage.get(req.headers.get('Accept-Language'));
   if (!lng) lng = fallbackLng;
 
   // パスのlanguagesがサポートされていない場合はリダイレクトします
+  // スターター部分の認証機能関連のpathはi18nに対応してないのでハードコーディングしている。
+  // 認証関連のパスはi18n機能に対応していない。
   if (
     req.nextUrl.pathname !== '/' && // ルートパスの場合はリダイレクトしない
-    !languages.some((loc) => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
+    req.nextUrl.pathname !== '/signin' &&
+    req.nextUrl.pathname !== '/signin/password_signin' &&
+    req.nextUrl.pathname !== '/auth/callback' &&
+    req.nextUrl.pathname !== '/account' &&
+    !languages.some(loc => req.nextUrl.pathname.startsWith(`/${loc}`)) &&
     !req.nextUrl.pathname.startsWith('/_next')
   ) {
-    return NextResponse.redirect(
-      new URL(`/${lng}${req.nextUrl.pathname}`, req.url)
-    );
+    return NextResponse.redirect(new URL(`/${lng}${req.nextUrl.pathname}`, req.url));
   }
 
   if (req.headers.has('referer')) {
     const referer = req.headers.get('referer');
     if (referer) {
       const refererUrl = new URL(referer);
-      const lngInReferer = languages.find((l) =>
-        refererUrl.pathname.startsWith(`/${l}`)
-      );
+      const lngInReferer = languages.find(l => refererUrl.pathname.startsWith(`/${l}`));
 
       const response = NextResponse.next();
       if (lngInReferer) response.cookies.set(cookieName, lngInReferer);
@@ -51,3 +52,4 @@ export async function middleware(req: NextRequest) {
 
   return NextResponse.next();
 }
+
