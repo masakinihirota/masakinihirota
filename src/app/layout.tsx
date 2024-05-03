7c836c19b4e5c77fed58bbb00e477b029c715598
masakinihirota <masakinihirota@gmail.com>
2024-05-04 08:19:01 +0900

    ダークモード 色設定の最低限完了

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -56,7 +56,7 @@ export async function generateMetadata(): Promise<Metadata> {
 export default async function RootLayout({ children, params: { lng = 'ja' } }) {
   return (
     <html lang={lng} dir={dir(lng)} suppressHydrationWarning>
-      <body className="bg-black loading">
+      <body className=" loading">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Navbar />
           <main
