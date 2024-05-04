83e7643342e6a474abe0ff99329efe1e80cb4edf
masakinihirota <masakinihirota@gmail.com>
2024-05-04 10:43:11 +0900

    chore: Update Navbar component to fix logo alignment issue

--- src/components/layout/Navbar/Navlinks.tsx
+++ src/components/layout/Navbar/Navlinks.tsx
@@ -10,7 +10,6 @@ import { getRedirectMethod } from '@/utils/auth-helpers/settings';
 
 import s from './Navbar.module.css';
 
-
 interface NavlinksProps {
   user?: any;
 }
@@ -23,7 +22,7 @@ export default function Navlinks({ user }: NavlinksProps) {
     <div className="relative flex flex-row justify-between py-4 align-center md:py-6">
       <div className="flex items-center flex-1">
         <Link href="/" className={s.logo} aria-label="Logo">
-        VNS.BLUE
+          VNS.BLUE
           {/* <Logo /> */}
         </Link>
         <nav className="ml-6 space-x-2 lg:block">
